da1fa54b3945b6f9cb8309ff22608ef0c10e4182
fclassvisions <fclassvisions@gmail.com>
2022-03-11 07:27:05 -0500

    ListBox setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, { Component } from "react"
-import "./App.css"
+import React, { Component } from "react";
+import { countryList } from "./components/util/countries";
+import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App"></div>
+    return (
+      <div className="App">
+        <div className="center"></div>
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
